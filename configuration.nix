381c2921fafc7ab67855b0953a51f39f76be809e
rPlakama <rPlakama@proton.me>
2025-12-28 11:06:05 -0300

    Removi e limpei um pouco do codigo.

--- configuration.nix
+++ configuration.nix
@@ -1,72 +1,53 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
+# Você não tá usando a variavel 'config'
+{ pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
-  # what the heck are you looking here
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  
 
-  #Hyprland 
+  #Hyprland
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
   };
 
-
-  #XDG portals
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
-  
 
-  #so lost, i'm faded
-  environment.systemPackages = [
-    pkgs.waybar
-    pkgs.kitty
-    pkgs.rofi
-    pkgs.swww
-    pkgs.firefox
-    pkgs.vlc
-    pkgs.discord
-    pkgs.nautilus
-    pkgs.btop
-    pkgs.git
-    pkgs.mako
-    pkgs.libnotify
-    pkgs.matugen
-    pkgs.pavucontrol
+  environment.systemPackages = with pkgs; [
+    waybar
+    kitty
+    rofi
+    swww
+    firefox
+    vlc
+    discord
+    nautilus
+    btop
+    git
+    mako
+    libnotify
+    matugen
+    pavucontrol
   ];
 
-
-
-  # Audio 
+  # Audio
   security.rtkit.enable = true;
   services.pipewire = {
-   enable = true;
+    enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     jack.enable = true;
     pulse.enable = true;
   };
 
-  
   nixpkgs.config.allowUnfree = true;
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "nixos";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
   # Set your time zone.
@@ -87,20 +68,25 @@
   };
   services.xserver.xkb = {
     layout = "br";
-    variant = "";
   };
   console.keyMap = "br-abnt2";
 
   users.users.vinicius = {
     isNormalUser = true;
     description = "vinicius";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    # Não precisa explicitar se o valor é null
   };
   security.sudo.enable = true;
   security.sudo.wheelNeedsPassword = false;
-  nix =  {
-    settings.experimental-features = ["nix-command" "flakes"];
+  nix = {
+    settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
   };
-  system.stateVersion = "25.11"; # Did you read the comment?
+  system.stateVersion = "25.11";
 }
--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { nixpkgs, ... }:
   let
     system = "x86_64-linux";
   in {
